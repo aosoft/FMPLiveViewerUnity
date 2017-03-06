a7779504083c294af7a56457669e3ae4c337548f
aosoft <tan-y@aosoft.jp>
2017-03-06 20:21:14 +0900

    FMP 制御実装中

--- Assets/FMPManager.cs
+++ Assets/FMPManager.cs
@@ -11,6 +11,8 @@ public class FMPManager : MonoBehaviour
 	RxFMPWork _fmpWork = null;
 
 	public StringReactiveProperty _playTime = new StringReactiveProperty();
+	public StringReactiveProperty _musicTitle = new StringReactiveProperty();
+	public StringReactiveProperty _musicCreator = new StringReactiveProperty();
 
 	public FMPManager()
 	{
@@ -23,6 +25,8 @@ public class FMPManager : MonoBehaviour
 		this.UpdateAsObservable().Subscribe(_ => _fmpWork.Update());
 
 		_fmpWork.PlayTime.Select(value => value.ToString()).Subscribe(value => _playTime.Value = value);
+		_fmpWork.MusicTitle.Subscribe(value => _musicTitle.Value = value);
+		_fmpWork.MusicCreator.Subscribe(value => _musicCreator.Value = value);
 	}
 
 	void OnDestroy()
--- Assets/UnityFMP/RxFMPWork.cs
+++ Assets/UnityFMP/RxFMPWork.cs
@@ -10,15 +10,28 @@ namespace UnityFMP
 	class RxFMPWork : IDisposable
 	{
 		private FMPWork _work = new FMPWork();
+		private FMPPartWork[] _tmpPartworks = new FMPPartWork[FMPPartWork.MaxChannelCount];
 		private uint? _musicStartCounter = new uint?();
 
 		private ReactiveProperty<TimeSpan> _propPlayTime = new ReactiveProperty<TimeSpan>();
 		private FloatReactiveProperty _propProgress = new FloatReactiveProperty();
 		private StringReactiveProperty _propMusicTitle = new StringReactiveProperty();
 		private StringReactiveProperty _propMusicCreator = new StringReactiveProperty();
+		private IntReactiveProperty _propActiveChannelCount = new IntReactiveProperty();
+		private IntReactiveProperty _propFMChannelCount = new IntReactiveProperty();
+		private IntReactiveProperty _propSSGChannelCount = new IntReactiveProperty();
+		private IntReactiveProperty _propPCMChannelCount = new IntReactiveProperty();
 		private ReactiveCollection<RxFMPPartWork> _propParts = new ReactiveCollection<RxFMPPartWork>();
 
 		private Subject<Unit> _observeMusicStart = new Subject<Unit>();
+
+		public RxFMPWork()
+		{
+			for (int i = 0; i < FMPPartWork.MaxChannelCount; i++)
+			{
+				_propParts.Add(new RxFMPPartWork());
+			}
+		}
 
 		public void Dispose()
 		{
@@ -53,6 +66,8 @@ namespace UnityFMP
 		private void InternalUpdate()
 		{
 			var gwork = _work.GetGlobalWork();
+			_work.CopyPartWork(0, _tmpPartworks, 0, _tmpPartworks.Length);
+
 			_propPlayTime.Value = TimeSpan.FromMilliseconds(gwork.PlayTime * 10);
 			if (gwork.Count < 1)
 			{
@@ -67,10 +82,55 @@ namespace UnityFMP
 				_propProgress.Value = (float)gwork.CountNow / (float)gwork.Count;
 			}
 
+			for (int i = 0; i < _tmpPartworks.Length; i++)
+			{
+				_propParts[i].Update(_tmpPartworks[i]);
+			}
+
 			if (_musicStartCounter.HasValue == false ||
 				_musicStartCounter.Value != gwork.StartCounter)
 			{
 				//	再生開始時に行うプロパティ更新
+
+				int fm = 0;
+				int ssg = 0;
+				int pcm = 0;
+				int pc = 0;
+				for (int i = 0; i < FMPPartWork.MaxChannelCount; i++)
+				{
+					var unit = gwork.Mode[i];
+					switch (unit)
+					{
+						case FMPSoundUnit.FM:
+							{
+								fm++;
+								pc++;
+							}
+							break;
+
+						case FMPSoundUnit.SSG:
+							{
+								ssg++;
+								pc++;
+							}
+							break;
+
+						case FMPSoundUnit.PCM:
+							{
+								pcm++;
+								pc++;
+							}
+							break;
+					}
+
+					_propParts[i].SetSoundUnit(unit);
+				}
+
+				_propActiveChannelCount.Value = pc;
+				_propFMChannelCount.Value = fm;
+				_propSSGChannelCount.Value = ssg;
+				_propPCMChannelCount.Value = pcm;
+
 
 				_propMusicTitle.Value = FMPControl.GetTextData(FMPText.Title);
 				_propMusicCreator.Value = FMPControl.GetTextData(FMPText.Creator);
@@ -111,6 +171,38 @@ namespace UnityFMP
 			get
 			{
 				return _propMusicCreator;
+			}
+		}
+
+		public IReadOnlyReactiveProperty<int> ActiveChannelCount
+		{
+			get
+			{
+				return _propActiveChannelCount;
+			}
+		}
+
+		public IReadOnlyReactiveProperty<int> FMChannelCount
+		{
+			get
+			{
+				return _propFMChannelCount;
+			}
+		}
+
+		public IReadOnlyReactiveProperty<int> SSGChannelCount
+		{
+			get
+			{
+				return _propSSGChannelCount;
+			}
+		}
+
+		public IReadOnlyReactiveProperty<int> PCMChannelCount
+		{
+			get
+			{
+				return _propPCMChannelCount;
 			}
 		}
 

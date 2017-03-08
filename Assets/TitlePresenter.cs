7051624d8b4117f83f23c5dbef805e52b1989190
aosoft <tan-y@aosoft.jp>
2017-03-08 13:58:50 +0900

    タイトルアニメーション実装中

---
+++ Assets/TitlePresenter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityFMP;
+using UniRx;
+
+public class TitlePresenter : MonoBehaviour
+{
+	public UnityEngine.UI.Text _title;
+	public UnityEngine.UI.Text _musicCreator;
+
+	// Use this for initialization
+	void Start()
+	{
+		FMPManager.FMPWork.MusicTitle.SubscribeToText(_title).AddTo(this);
+		FMPManager.FMPWork.MusicCreator.SubscribeToText(_musicCreator).AddTo(this);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}

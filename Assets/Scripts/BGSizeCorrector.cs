c63551c9064ff8e35db174235fcef5f3aec44a27
aosoft <tan-y@aosoft.jp>
2017-04-26 13:51:36 +0900

    背景対応中。背景用オブジェクトをスクリーンサイズに合わせてリサイズするようにした。

---
+++ Assets/Scripts/BGSizeCorrector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+/// <summary>
+/// アスペクト比を無視して全面リサイズする。
+/// </summary>
+public class BGSizeCorrector : MonoBehaviour
+{
+	public Camera _camera;
+
+	void Awake()
+	{
+		if (_camera != null && _camera.orthographic)
+		{
+			float height = _camera.orthographicSize * 2;
+
+			this.ObserveEveryValueChanged(x => new Tuple<int, int>(Screen.width, Screen.height))
+				.Subscribe(value =>
+				{
+					this.transform.localScale = new Vector3(
+						height * value.Item1 / value.Item2,
+						height, 1.0f);
+				}).AddTo(this);
+		}
+	}
+}
